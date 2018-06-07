f5cb323281a99c996be6051acaeb8350444e1204
Erick Guan <fantasticfears@gmail.com>
2018-06-07 18:09:52 +0200

    FIX: apply to new API

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-user-created-webhook
-# version: 1.0
+# version: 1.1
 # authors: Erick Guan (fantasticfears@gmail.com)
 
 PLUGIN_NAME = 'discourse_user_created_webhook'.freeze
@@ -10,7 +10,7 @@ after_initialize do
   register_seedfu_fixtures(Rails.root.join("plugins", "discourse-user-created-webhook", "db", "fixtures").to_s)
 
   DiscourseEvent.on(:user_created) do |user|
-    WebHook.enqueue_hooks(:user, user_id: user.id, event_name: 'user_created')
+    WebHook.enqueue_object_hooks(:user, user, 'user_created')
   end
 
 end
